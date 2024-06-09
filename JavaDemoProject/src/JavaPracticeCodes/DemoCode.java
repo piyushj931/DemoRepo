e550a3f3fc99a35200ff407085d584d54592937d
pjain <pjain@ljt40qn3-ban.int.openbet.com>
2024-06-09 09:20:45 +0530

    first code commit

---
+++ JavaDemoProject/src/JavaPracticeCodes/DemoCode.java
@@ -0,0 +1,17 @@
+package JavaPracticeCodes;
+
+public class DemoCode {
+	public static void main(String[] args)
+	{
+		
+		DemoCode d1 = new DemoCode();
+		d1.test();
+		
+	}
+	
+	public void test()
+	{
+		System.out.println("testing");
+	}
+
+}
